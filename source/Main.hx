ed4cb43f56712150c7ade8d88ad56944635572ab
dumbassosolol <ccager7@gmail.com>
2026-08-13 15:48:03 -0400

    oh the windows color mode thiingy

--- source/Main.hx
+++ source/Main.hx
@@ -79,7 +79,7 @@ class Main extends Sprite
 		FlxG.autoPause = false;
 		FlxG.mouse.visible = false;
 		#end
-		
+
 		#if CRASH_HANDLER
 		Lib.current.loaderInfo.uncaughtErrorEvents.addEventListener(UncaughtErrorEvent.UNCAUGHT_ERROR, onCrash);
 		#end
