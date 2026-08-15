2a2ef552637af21fae1d56c05d81b6c3cdcdc2c2
Emerald <guyanon71@gmail.com>
2026-08-14 21:13:59 -0400

    Create templateState.lua

---
+++ example_mods/states/scripts/templateState.lua
@@ -0,0 +1,39 @@
+function onBack() -- when you press escape
+    goToMainMenu()
+end
+
+function onUp() -- when you press up for ui
+    
+end
+
+function onDown() -- when you press down for ui
+    
+end
+
+function onAcceptPre() -- THE MOMENT YOU PRESS ENTER
+    
+end
+
+function onAccept() -- when you press enter
+    
+end
+
+function onAcceptPost() -- after you press enter
+    
+end
+
+function onCreate()
+    
+end
+
+function onUpdate(delta)
+    
+end
+
+function onBeatHit()
+    
+end
+
+function onStepHit()
+    
+end
